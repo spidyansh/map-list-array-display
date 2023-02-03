d64d34e02cae351e05cd39c93873f20d8e3fe265
Anshul Aggarwal <aggarwalanshul1997@gmail.com>
2023-02-03 23:47:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ShowHide from "./ShowHide";
 
+const data = [
+  { name: "anshul", age: "30", id: "0" },
+  { name: "rahul", age: "10", id: "1" },
+  { name: "sanjay", age: "40", id: "2" },
+];
+
+const display = data.map((item) =><li>{item.name}</li>);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>{display}</ul>
+      <ShowHide/>
+    </>
   );
 }
 
---
+++ src/ShowHide.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ShowHide() {
+
+    const togglefunc=()=>{
+        return(<h2>hello</h2>)}
+    
+  return (
+    <>
+        <h1 className='container'>Anshul Aggarwal</h1>
+        <button onClick={togglefunc}>Hide Element</button>
+    </>
+  )
+}
